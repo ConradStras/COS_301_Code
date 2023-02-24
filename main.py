936654cd76abae80001c34b11ead5ae1a9a21d27
ConradStras <cadstrasheim@gmail.com>
2023-02-24 11:18:10 +0200

    Added a loop that adds numbers up until i, Following: x = 1+2+3+...+i

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ def print_hi(name):
 def summer(num1, num2):
     print(num1 + num2)
 
+def randomLoop(num):
+    total = 0
+    for i in range(0, num):
+        total += i
+    return total
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Conrad')
     summer(20, 21)
+    print(randomLoop(8))
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
